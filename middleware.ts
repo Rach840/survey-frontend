8cfce7fe59321fb50e9e0bb3b23c2958ec3e53a4
Rach Balayan <rach.balayan1@gmail.com>
2025-10-22 07:42:38 +0300

    add icon

--- middleware.ts
+++ middleware.ts
@@ -18,7 +18,7 @@ export function middleware(req: NextRequest) {
 
 export const config = {
     matcher: [
-        '/((?!_next/static|_next/image|favicon.ico|robots.txt|sitemap.xml).*)',
+        '/((?!_next/static|_next/image|favicon.svg|robots.txt|sitemap.xml).*)',
     ],
 }
 
